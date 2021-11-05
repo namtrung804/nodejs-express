7373652df8656c425cfd07cfaef412bae25668e9
NamTrung <trung.tn@techgel.com>
2021-11-05 17:05:04 +0700

    Update alpine 16

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -t "namtrung/nodejs-express:lts-alpine" .
-docker push "namtrung/nodejs-express:lts-alpine"
+docker build -t "namtrung/nodejs-express:alpine-16" .
+docker push "namtrung/nodejs-express:alpine-16"
